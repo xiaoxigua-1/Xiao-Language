b70c00e59144755967358f198573c960d0f689d4
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2022-01-01 16:58:16 +0800

    parse class test

--- src/test/kotlin/ParserTest.kt
+++ src/test/kotlin/ParserTest.kt
@@ -1,4 +1,5 @@
 import xiaoLanguage.ast.ASTNode
+import xiaoLanguage.ast.Class
 import xiaoLanguage.ast.Function
 import xiaoLanguage.ast.Import
 import xiaoLanguage.lexer.Lexer
@@ -9,6 +10,9 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ParserTest {
+    /*
+        parse to Abstract Syntax Tree
+    */
     private fun parserTest(filePath: String): MutableList<ASTNode> {
         val file = File(this::class.java.getResource(filePath)!!.path)
         val stringStream = StringStream(file)
@@ -17,39 +21,45 @@ class ParserTest {
         val (ast, reports) = parser.parser()
 
         for (report in reports) {
-            report.printReport(file.readLines(), "/import/import.xiao")
+            report.printReport(file.readLines(), filePath)
         }
 
         return ast
     }
 
+    /*
+        The correctness of parse import
+    */
     @Test
     fun parserImportTest() {
         val expectedData = listOf("xiao/Math", "test2", "xiao/Math/plus")
         val ast = parserTest("/import/import.xiao")
 
-        ast.mapIndexed { index ,node ->
+        ast.mapIndexed { index, node ->
             if (node is Import) {
                 val path = node.path.joinToString("/") { it.literal }
 
-                assertEquals(expectedData[index] , path)
+                assertEquals(expectedData[index], path)
             }
         }
     }
 
+    /*
+        The correctness of parse function
+    */
     @Test
     fun parserFunctionTest() {
         val expectedDataName = listOf("main", "test23", "test33", "test34", "test66")
         val expectedDataArgsName = listOf(null, null, listOf("name"), listOf("name", "info"), null)
         val expectedDataArgsType = listOf(null, null, listOf("Str"), listOf("Str", "Int"), null)
         val expectedDataReturnType = listOf(null, null, null, null, "Str")
-        val ast = parserTest("/functions/functions.xiao")
+        val ast = parserTest("/functions/function.xiao")
 
-        ast.mapIndexed { index ,node ->
+        ast.mapIndexed { index, node ->
             if (node is Function) {
                 val name = node.functionName.literal
 
-                assertEquals(expectedDataName[index] , name)
+                assertEquals(expectedDataName[index], name)
 
                 node.parameters.mapIndexed { index2, parameter ->
                     assertEquals(expectedDataArgsName[index]?.get(index2), parameter.name.literal)
@@ -57,6 +67,21 @@ class ParserTest {
                 }
 
                 assertEquals(expectedDataReturnType[index], node.returnType?.typeTokens?.literal)
+            }
+        }
+    }
+
+    /*
+        The correctness of parse class
+     */
+    @Test
+    fun parserClassTest() {
+        val ast = parserTest("/classes/class.xiao")
+        val expectedDataName = listOf("A", "B12", "ABC")
+
+        ast.mapIndexed { index, astNode ->
+            if (astNode is Class) {
+                assertEquals(expectedDataName[index], astNode.className.literal)
             }
         }
     }
