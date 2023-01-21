a6f71d8aed93103e5ec10e71c68149aa16c97bb0
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2023-01-21 06:20:41 +0000

    init parser fram

--- libs/parser/src/main/kotlin/xiao/language/parser/Parser.kt
+++ libs/parser/src/main/kotlin/xiao/language/parser/Parser.kt
@@ -1,17 +1,35 @@
 package xiao.language.parser
 
 import xiao.language.lexer.Lexer
+import xiao.language.parser.syntax.*
+import xiao.language.utilities.Token
+import xiao.language.utilities.ast.Statement
+import xiao.language.utilities.ast.Visibility
+import xiao.language.utilities.tokens.Keywords
+import xiao.language.utilities.tokens.Tokens
 
-data class Parser(val lexer: Lexer): Iterator<String> {
-    override fun hasNext(): Boolean {
-        TODO("Not yet implemented")
-    }
+data class Parser(val lexer: Lexer): Iterator<Statement> {
+    private var isEOF = false
 
-    override fun next(): String {
-        TODO("Not yet implemented")
+    override fun hasNext(): Boolean = !isEOF
+
+    override fun next(): Statement {
+        TODO("next statement")
     }
 }
 
-fun Parser.nextExpression() {
+fun Parser.nextStatement() {
+    if (lexer.hasNext()) {
+        val token = lexer.next()
+        when (val type = token.type) {
+            is Tokens.Keyword -> keyword(type.type, token)
+        }
+    } else {
+    }
+}
 
+private fun Parser.keyword(kwdType: Keywords, kwd: Token) {
+    when(kwdType) {
+        Keywords.Fn -> function(Visibility.Private, kwd)
+    }
 }
--- libs/parser/src/main/kotlin/xiao/language/parser/syntax/function.kt
+++ libs/parser/src/main/kotlin/xiao/language/parser/syntax/function.kt
@@ -1,2 +1,9 @@
 package xiao.language.parser.syntax
 
+import xiao.language.parser.Parser
+import xiao.language.utilities.Token
+import xiao.language.utilities.ast.Visibility
+
+fun Parser.function(vis: Visibility, kwd: Token) {
+
+}
