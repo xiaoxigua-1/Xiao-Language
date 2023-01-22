afd9294c6053e2477f014e6b7891b48c8ba0d6c5
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2023-01-22 06:14:46 +0000

    parser: add function

--- libs/parser/src/main/kotlin/xiao/language/parser/Parser.kt
+++ libs/parser/src/main/kotlin/xiao/language/parser/Parser.kt
@@ -5,10 +5,12 @@ import xiao.language.parser.syntax.*
 import xiao.language.utilities.Token
 import xiao.language.utilities.ast.Statement
 import xiao.language.utilities.ast.Visibility
+import xiao.language.utilities.exceptions.EOFException
 import xiao.language.utilities.tokens.Keywords
 import xiao.language.utilities.tokens.Tokens
+import java.lang.Exception
 
-data class Parser(val lexer: Lexer): Iterator<Statement> {
+data class Parser(val lexer: Lexer) : Iterator<Statement> {
     private var isEOF = false
 
     override fun hasNext(): Boolean = !isEOF
@@ -23,13 +25,23 @@ fun Parser.nextStatement() {
         val token = lexer.next()
         when (val type = token.type) {
             is Tokens.Keyword -> keyword(type.type, token)
+            else -> {}
         }
     } else {
     }
 }
 
+internal fun Parser.expect(type: Tokens, exception: Exception): Token {
+    for (token in lexer) {
+        return if (token.type == type) token
+        else break
+    }
+    throw exception
+}
+
 private fun Parser.keyword(kwdType: Keywords, kwd: Token) {
-    when(kwdType) {
+    when (kwdType) {
         Keywords.Fn -> function(Visibility.Private, kwd)
+        else -> {}
     }
 }
--- libs/parser/src/main/kotlin/xiao/language/parser/syntax/function.kt
+++ libs/parser/src/main/kotlin/xiao/language/parser/syntax/function.kt
@@ -1,9 +1,42 @@
 package xiao.language.parser.syntax
 
 import xiao.language.parser.Parser
+import xiao.language.parser.expect
 import xiao.language.utilities.Token
+import xiao.language.utilities.ast.Parameter
+import xiao.language.utilities.ast.Statement
 import xiao.language.utilities.ast.Visibility
+import xiao.language.utilities.exceptions.ExpectException
+import xiao.language.utilities.tokens.Delimiters
+import xiao.language.utilities.tokens.Punctuations
+import xiao.language.utilities.tokens.Tokens
 
 fun Parser.function(vis: Visibility, kwd: Token) {
+    val name = expect(Tokens.Identifier, ExpectException("Missing function name.", "Identifier"))
+    val params = parameter()
+    Statement.Function(vis, kwd, name, params)
+}
 
+fun Parser.parameter(): List<Parameter> {
+    val left = expect(Tokens.Delimiter(Delimiters.LeftParentheses), ExpectException("Missing left parentheses.", "("))
+    val params = mutableListOf<Parameter>()
+    var isComma = false
+
+    for (token in lexer) {
+        val type = token.type
+        isComma = when {
+            type is Tokens.Delimiter && type.type == Delimiters.RightParentheses -> return params
+            type is Tokens.Punctuation && type.punctuation == Punctuations.Comma && !isComma -> true
+            else -> {
+                val name = expect(Tokens.Identifier, ExpectException("Missing parameter name.", "Identifier"))
+                val colon = expect(Tokens.Punctuation(Punctuations.Colon), ExpectException("Missing Colon.", ":"))
+                val parameterType = expect(Tokens.Identifier, ExpectException("Missing parameter type.", "Identifier"))
+
+                params.add(Parameter(name, colon, parameterType))
+                false
+            }
+        }
+    }
+
+    throw ExpectException("Unclosed delimiter", "(")
 }
