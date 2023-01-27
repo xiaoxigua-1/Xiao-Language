16d1ff90b1206319adadcbcc848ec09bf43ce3ab
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2023-01-27 11:03:30 +0000

    lexer: fix peek bug

--- libs/lexer/src/main/kotlin/xiao/language/lexer/Lexer.kt
+++ libs/lexer/src/main/kotlin/xiao/language/lexer/Lexer.kt
@@ -33,8 +33,8 @@ data class Lexer(
 
     fun peek(): Token {
         return peek ?: run {
-            peek = next()
-            peek!!
+            peek = nextToken()
+            peek ?: Token(Tokens.EOF, '\u0000', Span(fileStream.index))
         }
     }
 }
